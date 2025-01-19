e81e01a0dfb6a0b2f9d18948d45eb76a762ba1db
arina kovshik <144607655+lililupa@users.noreply.github.com>
2025-01-19 19:20:09 +0300

    xml

--- src/main/java/com/example/webpp/controller/ZipController.java
+++ src/main/java/com/example/webpp/controller/ZipController.java
@@ -14,45 +14,44 @@ import java.util.zip.ZipOutputStream;
 public class ZipController {
     @GetMapping ("/zip")
     public String controller(){
+        zipWrite();
+
+        zipRead();
+        return "test"  ;
+    }
+
+    private static void zipWrite() {
         File filename = new File("src/main/resources/in.txt");
-        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("C:\\Users\\I\\IdeaProjects\\WebPP\\src\\main\\resources\\out.zip"));
+        try(ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("src/main/resources/out.zip"));
             FileInputStream fis= new FileInputStream(filename);)
         {
             ZipEntry entry1=new ZipEntry("src/main/resources/in.txt");
             zout.putNextEntry(entry1);
-            // считываем содержимое файла в массив byte
             byte[] buffer = new byte[fis.available()];
             fis.read(buffer);
-            // добавляем содержимое к архиву
             zout.write(buffer);
-            // закрываем текущую запись для новой записи
             zout.closeEntry();
         }
         catch(Exception ex){
 
             System.out.println(ex.getMessage());
         }
-
-        extracted();
-        return "test"  ;
     }
 
-    private static void extracted() {
-        File filename1 = new File("C:\\Users\\I\\IdeaProjects\\WebPP\\src\\main\\resources\\out.zip");
+    private static void zipRead() {
+        File filename1 = new File("src/main/resources/out.zip");
         try(ZipInputStream zin = new ZipInputStream(new FileInputStream(filename1)))
         {
             ZipEntry entry;
             String name;
             while((entry=zin.getNextEntry())!=null){
 
-                name = entry.getName(); // получим название файла
+                name = entry.getName();
                 System.out.printf("File name: %s \n", name);
 
-                // распаковка
-                FileOutputStream fout = new FileOutputStream("new" + name);
+                FileOutputStream fout = new FileOutputStream("src/main/resources/out.txt");
                 for (int c = zin.read(); c != -1; c = zin.read()) {
-//                    fout.write(c);
-                    System.out.println(c);
+                    fout.write(c);
                 }
                 fout.flush();
                 zin.closeEntry();
